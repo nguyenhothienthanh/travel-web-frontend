7da4225f62b7012dfee34e9c844021e9792bce36
VK <nguyenvietkhoa003@gmail.com>
2025-08-13 01:12:33 +0700

    reponsive login and signup page, create Router web

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-
-import './App.css'
-import SearchBar from './components/home/SearchBar'
-import TopCategory from './components/home/TopCategory'
-import TravelCard from './components/home/TravelCard/TravelCard'
-import Footer from './components/layout/Footer'
-import Header from './components/layout/Header'
-import DetailsPage from './pages/DetailsPage'
-import Home from './pages/Home'
-import ListPage from './pages/ListPage'
-import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/SignupPage'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import './App.css';
+import DetailsPage from './pages/DetailsPage';
+import Home from './pages/Home';
+import ListPage from './pages/ListPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 function App() {
 
   return (
-    <>
-      <SignupPage/>
-    </>
-      
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<DetailsPage />} />
+        <Route path="/list" element={<ListPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
